6ce8c4ecf6f57335eeb76f58b63d02b391cb6170
stabash <natashareedbrewer@googlemail.com>
2022-05-04 12:19:35 +0200

    added ngFor structural directive, which will loop through the todos property in the componant class, imported todo model to todo component, created todo property.

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Todo } from '../shared/todo.model';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
 })
 export class TodosComponent implements OnInit {
+  todos!: Todo[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
