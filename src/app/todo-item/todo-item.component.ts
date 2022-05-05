27bc6974145040adef3e070a04278bfa35c8952a
stabash <natashareedbrewer@googlemail.com>
2022-05-05 11:57:35 +0200

    Inserted todo-item selector/tag to todos html Copied second div from todos component and added to new todo-item component. Replaced with a todo-item selector, pasted second div to new todo-item html.

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../shared/todo.model';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.scss']
+  styleUrls: ['./todo-item.component.scss'],
 })
 export class TodoItemComponent implements OnInit {
+  todo!: Todo;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
